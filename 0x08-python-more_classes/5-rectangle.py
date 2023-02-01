c54c0362da7ed4b124571cd58b43e78fbfcc510f
legacycodine <adebimpee842@gmail.com>
2023-02-01 04:02:26 -0800

    5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -3,92 +3,60 @@
 
 
 class Rectangle:
-"""Definition a class rectangle."""
+    """Definition a class rectangle."""
 
+    def __init__(self, width=0, height=0):
+        """Initializing Rectangle.
+        """
+        self.width = width
+        self.height = height
 
-def __init__(self, width=0, height=0):
-"""Initializing Rectangle.
-"""
-self.width = width
-self.height = height
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
 
+    @width.setter
+    def width(self, value):
+        if not type(int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-@property
-def width(self):
-"""Get/set the width of the rectangle."""
-return self.__width
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
 
-@width.setter
-def width(self, value):
-if not type(int):
-raise TypeError("width must be an integer")
+    @height.setter
+    def height(self, value):
+        if not type(int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
-if value < 0:
-raise ValueError("width must be >= 0")
-self.__width = value
+    def area(self):
+        """Calculate the area of the rectangle."""
+        return self.width * self.height
 
-@property
-def height(self):
-"""Get/set the height of the rectangle."""
-return self.__height
+    def perimeter(self):
+        """Calculate the perimeter of the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return 0
+            return (self.width + self.height) * 2
 
-@height.setter
-def height(self, value):
+    def __str__(self):
+        """Print the rectangle."""
+        if self.width == 0 or self.height == 0:
+            return ""
+        return ((("#" * self.width) + "\n") * self.height)[:-1]
 
-							if not type(int):
+    def __repr__(self):
+        """Print the rectangle using eval."""
+        return "Rectangle({}, {})".format(self.width, self.height)
 
-								raise TypeError("height must be an integer")
-
-								if value < 0:
-
-								raise ValueError("height must be >= 0")
-
-								self.__height = value
-
-
-
-								def area(self):
-
-									"""Calculate the area of the rectangle."""
-
-									return self.width * self.height
-
-
-
-									def perimeter(self):
-
-										"""Calculate the perimeter of the rectangle."""
-
-										if self.width == 0 or self.height == 0:
-
-										return 0
-
-										return (self.width + self.height) * 2
-
-
-
-										def __str__(self):
-
-											"""Print the rectangle."""
-
-											if self.width == 0 or self.height == 0:
-
-											return ""
-
-											return ((("#" * self.width) + "\n") * self.height)[:-1]
-
-
-
-											def __repr__(self):
-
-												"""Print the rectangle using eval."""
-
-												return "Rectangle({}, {})".format(self.width, self.height)
-
-
-
-												def __del__(self):
-
-													"""Print a message when the rectangle is deleted."""
-
-													print("Bye rectangle...")
+    def __del__(self):
+        """Print a message when the rectangle is deleted."""
+        print("Bye rectangle...")
