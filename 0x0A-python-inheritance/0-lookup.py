c41e21fc4aa77087be5f17c7b5d7699ace1efb2d
legacycodine <adebimpee842@gmail.com>
2023-02-06 05:07:41 -0800

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function that returns the list
+"""
+
+
+def lookup(obj):
+    """
+    List of available attribute and methods of an object
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+module: 2-is_same_class
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    functon: is_kind_of_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
+    """
+
+    return isinstance(obj, a_class)
