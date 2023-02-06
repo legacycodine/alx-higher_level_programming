5fab1ae85d08ba763c8ddadeb8a25b14921b2e88
legacycodine <adebimpee842@gmail.com>
2023-02-06 03:01:10 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/tests/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines a class-checking functon.
+"""
+
+
+def is_same_class(obj, a_class):
+    """Check if an obj is exactly an instance of a given class.
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is exactly an instance of a_class - True.
+        Otherwise - False.
+    """
+    return type(obj) == a_class
