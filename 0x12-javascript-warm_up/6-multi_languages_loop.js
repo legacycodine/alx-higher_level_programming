c2dbd9fa3caf0f7d07525704d664b402fb903478
legacycodine <adebimpee842@gmail.com>
2023-04-11 07:34:07 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-const myVar = parseInt(process.argv[2]);
-if (process.argv[2] === undefined || isNaN(myVar)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + myVar);
+const myStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let idx = 0;
+
+while (idx < 3) {
+  console.log(myStr[idx]);
+  idx++;
 }
