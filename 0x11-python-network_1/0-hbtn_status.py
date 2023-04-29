abd6e57982a675feaddcd3d62980caf9e8fcd211
legacycodine <adebimpee842@gmail.com>
2023-04-29 11:16:49 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,27 +1,12 @@
-#!/usr/bin/env python3
-
-import urllib.request
-
-
-def fetch_url(url):
-    with urllib.request.urlopen(url) as response:
-        content = response.read().decode('utf-8')
-        return content
-
-
-def print_response_info(content):
-    print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
-    print(f"\t- utf8 content: {content}")
-
-
-def main():
-    url = 'https://alx-intranet.hbtn.io/status'
-    content = fetch_url(url)
-    print_response_info(content)
-
-
-if __name__ == '__main__':
-    main()
-
+#!/usr/bin/python3
+"""
+fetches https://alx-intranet.hbtn.io/status
+"""
+if __name__ == "__main__":
+    import urllib.request as request
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+        html = r.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
